a69685eae39bb4b43277c182abbd813972a1aa0b
WoolenStone <wph2012.wh@gmail.com>
2024-01-15 16:29:35 -0600

    Fixed pokeEntry functionality
    Fixed the fetch requests and completed moving pokeInfo into pokeEntry

--- src/Pages/PokeEntry.jsx
+++ src/Pages/PokeEntry.jsx
@@ -1,13 +1,35 @@
-import { useLoaderData, Link } from "react-router-dom"
-import PokeInfo from "../Components/pokeInfo"
+import { useLoaderData } from "react-router-dom"
+import PokeBasicInfo from "../Components/PokeBasicInfo"
+import PokeForm from "../Components/PokeForm"
+import PokeVar from "../Components/PokeVar"
 
 //Takes pokemon api URL and returns an organized structure of the pokemons information
 export default function PokeEntry() {
-    const pokeInfo = useLoaderData()
+    const [pokeInfo, pokeSpecies] = useLoaderData()
+
+    const engIndex = () => {
+        var index = 0
+        while (pokeSpecies.flavor_text_entries && pokeSpecies.flavor_text_entries[index].language.name != "en") {
+            index++
+        }
+        return index
+    }
 
     return (
         <>
-            <PokeInfo pokemon={pokeInfo}/>
+            <PokeBasicInfo imgURL={pokeInfo.sprites.front_default} name={"Name: " + pokeInfo.name} typings={pokeInfo.types}/>
+            <p className="description">Description: {pokeSpecies && pokeSpecies.flavor_text_entries.length > 0 && pokeSpecies.flavor_text_entries[engIndex()].flavor_text.replaceAll('', '\n')}</p>
+            <section className="forms">
+                Forms:
+                {pokeInfo.forms.length > 1 ?
+                    pokeInfo.forms.map(form => <PokeForm key={form.name} formURL={form.url}/>) : " None"
+            }
+            </section>
+            <section className="varients">
+                Variations:
+                {(pokeSpecies && pokeSpecies.varieties.length > 1) ? 
+                pokeSpecies.varieties.map(varient => <PokeVar key={varient.pokemon.name} origName={pokeInfo.name} varientURL={varient.pokemon.url}/>) : " None"}
+            </section>
         </>
     )
 }
@@ -16,7 +38,14 @@ export default function PokeEntry() {
 export const pokeLoader = async ({ params }) => {
     const URL = 'https://pokeapi.co/api/v2/pokemon'
     const { pokemon } = params
-    const res = await fetch(`${URL}/${pokemon}`)
 
-    return res.json()
+    const [res1, res2] = await Promise.all([
+        fetch(`${URL}/${pokemon}`),
+        fetch(`${URL}-species/${pokemon}`)
+    ])
+    
+    const pokeInfo = await res1.json()
+    const pokeSpec = await res2.json()
+
+    return [pokeInfo, pokeSpec]
 }
